3cf75924f5ef492a027bfcbf47027c0340cdbd0b
Ramiro-boop <fpbramirobenitez@gmail.com>
2025-07-26 17:39:51 -0300

    Add files via upload

---
+++ Ventanas/Primer_Ventana/Gestor.java
@@ -0,0 +1,12 @@
+package EjercicioVtn1;
+
+public class Gestor {
+
+	public static void main(String[] args) {
+		
+		vtnDePrueba vtnPrueba = new vtnDePrueba();
+		vtnPrueba.setVisible(true);
+
+	}
+
+}
